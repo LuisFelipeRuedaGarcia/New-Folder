ad886ab8a7dd49fd8c8c9d4e6836f67db8bfde40
Luis Felipe Rueda García <lfxrg0724@gmail.com>
2023-03-09 14:18:18 -0500

    feat: :ambulance: commit 002 (6)
    nhfhnhng

--- js.js
+++ js.js
@@ -1,5 +1,25 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 //01
 =======
 //001(5)
 >>>>>>> 109d179 (feat: commit 001(5))
+=======
+//002(6)
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+// app.listen(3000, function () { console = console.log });
+>>>>>>> ef64cad (feat: :ambulance: commit 002 (6))
